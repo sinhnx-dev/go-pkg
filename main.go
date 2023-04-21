73794a525c50b076650ceea070ce419e3fd15acd
sinhnx <contact@sinhnx.dev>
2023-04-21 23:44:39 +0700

    go-pkg build

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/sinhnx-dev/go-pkg/numbers"
+)
+
+func main() {
+	num := 13
+	if numbers.IsPrime(num) {
+		fmt.Printf("%d is prime\n", num)
+	} else {
+		fmt.Printf("%d is not prime\n", num)
+	}
 }
